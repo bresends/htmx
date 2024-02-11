834bd48eee69fa4ae06cd81681f80a0f2d52b730
Bruno <bruno.resendesantos45@gmail.com>
2024-02-11 11:05:12 -0300

    feat: add devpleno schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,6 +1,42 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+
+import {
+    pgTable,
+    serial,
+    varchar,
+    text,
+    integer,
+    timestamp,
+    boolean,
+} from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
-    fullName: text('full_name'),
+    email: varchar('email', { length: 256 }).notNull().unique(),
+    password: varchar('password', { length: 256 }).notNull(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+    todos: many(todos),
+}));
+
+export const todos = pgTable('todos', {
+    id: serial('id').primaryKey(),
+    todo: varchar('todo').notNull(),
+    content: text('content'),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+    done: boolean('done').default(false),
+    userId: integer('user_id')
+        .references(() => users.id, {
+            onDelete: 'cascade',
+        })
+        .notNull(),
+});
+
+export const todosRelations = relations(todos, ({ one }) => ({
+    user: one(users, {
+        fields: [todos.userId],
+        references: [users.id],
+    }),
+}));

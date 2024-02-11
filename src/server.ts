c8d8a2121fc6ed7a1499c63e400b16d0c2a430b9
Bruno <bruno.resendesantos45@gmail.com>
2024-02-11 11:06:03 -0300

    feat: add liquid templating

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 import { env } from '../env';
+import { db } from './database/db';
+import { count } from 'drizzle-orm';
+import { todos } from './database/schema';
+import { Liquid } from 'liquidjs';
 
 const app = express();
 
+app.engine('liquid', new Liquid().express());
+app.set('views', './src/views');
+app.set('view engine', 'liquid');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).render('index', { value: 'Hi. Hello' });
+});
+
+app.get('/db', async (req, res) => {
+    const totalTodos = await db.select({ value: count(todos.id) }).from(todos);
+    res.status(200).send({ totalTodos });
 });
 
 app.listen(env.PORT, () => {

5801397d15eb90bf1dbd0709c318ffc6447e2059
Bruno <bruno.resendesantos45@gmail.com>
2024-02-11 17:46:20 -0300

    feat: user creation added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { env } from '../env';
 import { db } from './database/db';
-import { count } from 'drizzle-orm';
-import { todos } from './database/schema';
+import { users } from './database/schema';
 import { Liquid } from 'liquidjs';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.set('views', './src/views');
 app.set('view engine', 'liquid');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     if (req.headers['hx-boosted'])
@@ -34,6 +37,13 @@ app.get('/signin', (req, res) => {
     res.status(200).render('signin');
 });
 
+app.post('/signin', async (req, res) => {
+    if (req.headers['hx-boosted'])
+        return res.status(200).render('partials/signin');
+
+    res.status(200).render('signin');
+});
+
 app.get('/signup', (req, res) => {
     if (req.headers['hx-boosted'])
         return res.status(200).render('partials/signup');
@@ -41,9 +51,41 @@ app.get('/signup', (req, res) => {
     res.status(200).render('signup');
 });
 
-app.get('/db', async (req, res) => {
-    const totalTodos = await db.select({ value: count(todos.id) }).from(todos);
-    res.status(200).send({ totalTodos });
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    res.cookie('session', email, { maxAge: 60 * 60 * 24 * 30, httpOnly: true });
+    await db.insert(users).values({
+        email,
+        password,
+    });
+
+    if (req.headers['hx-boosted']) {
+        res.header('hx-redirect', '/app');
+        return res.sendStatus(201);
+    }
+
+    return res.redirect(301, '/app');
+});
+
+app.get('/logout', (req, res) => {
+    res.cookie('session', '', { maxAge: 0, httpOnly: true });
+
+    if (req.headers['hx-boosted']) {
+        res.header('hx-redirect', '/signin');
+        return res.sendStatus(200);
+    }
+
+    return res.redirect(301, '/signin');
+});
+
+app.get('/app', async (req, res) => {
+    const allUsers = await db.select().from(users);
+
+    res.status(200).render('app/index', {
+        allUsers,
+        email: req.cookies.session,
+    });
 });
 
 app.listen(env.PORT, () => {

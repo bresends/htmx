1ff3f3f69aad750699f590e8e8b3d227ab5f95bd
bruno <bruno.resende@gmx.com>
2024-02-12 18:27:07 -0300

    feat: add contacts CRUD

--- src/router/contacts/index.ts
+++ src/router/contacts/index.ts
@@ -1,27 +1,23 @@
 import { Router } from 'express';
 import { db } from '@src/database/db';
 import { users } from '@src/database/schema';
-import { like } from 'drizzle-orm';
+import { eq, ilike } from 'drizzle-orm';
+import { z } from 'zod';
 
 export const contacts = Router();
 
 contacts.get('/', async (req, res) => {
     const query = req.query.q;
+    const searchValue = query || '';
 
-    if (!query) {
-        const allUsers = await db.select().from(users);
-        return res
-            .status(200)
-            .render('contacts/app/index', { contacts: allUsers });
-    }
     const filteredUsers = await db
         .select()
         .from(users)
-        .where(like(users.name, `%${query}%`));
+        .where(ilike(users.name, `%${searchValue}%`));
 
     return res
         .status(200)
-        .render('contacts/app/index', { contacts: filteredUsers });
+        .render('contacts/app/index', { contacts: filteredUsers, searchValue });
 });
 
 contacts.get('/new', (req, res) => {
@@ -29,15 +25,101 @@ contacts.get('/new', (req, res) => {
 });
 
 contacts.post('/new', async (req, res) => {
-    const { name, email, password } = req.body;
+    const contactSchema = z.object({
+        email: z.string().email(),
+        name: z
+            .string()
+            .min(5, { message: 'Name must be at least 5 characters long' }),
+        password: z
+            .string()
+            .min(8, { message: 'Password must be at least 8 characters long' }),
+    });
+    try {
+        // Validate request body against the schema
+        const { email, name, password } = contactSchema.parse(req.body);
+        await db.insert(users).values({
+            name,
+            password,
+            email,
+        });
+        return res.redirect(301, '/contacts');
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            if (error instanceof z.ZodError) {
+                res.status(400).render('contacts/new', {
+                    errors: error.errors,
+                    formData: req.body,
+                });
+            }
+        } else {
+            console.error('Error creating contact:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    }
+});
 
-    if (name.length < 5)
-        return res.render('/contacts/app/new', { contacts: name });
+contacts.get('/:contact_id/edit', async (req, res) => {
+    const contactId = req.params.contact_id;
 
-    // await db.insert(users).values({
-    //     name,
-    //     password,
-    //     email,
-    // });
+    const user = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, Number(contactId)));
+
+    return res.status(200).render('contacts/edit', { contact: user[0] });
+});
+
+contacts.post('/:contact_id/edit', async (req, res) => {
+    const contactId = req.params.contact_id;
+
+    const contactSchema = z.object({
+        email: z.string().email(),
+        name: z
+            .string()
+            .min(5, { message: 'Name must be at least 5 characters long' }),
+        password: z
+            .string()
+            .min(8, { message: 'Password must be at least 8 characters long' }),
+    });
+
+    try {
+        const { email, name, password } = contactSchema.parse(req.body);
+        await db.update(users).set({
+            name,
+            password,
+            email,
+        });
+        return res.redirect(301, `/contacts/${contactId}`);
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            if (error instanceof z.ZodError) {
+                res.status(400).render(`/contacts/${contactId}/edit`, {
+                    errors: error.errors,
+                    formData: req.body,
+                });
+            }
+        } else {
+            console.error('Error creating contact:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    }
+});
+
+contacts.get('/:contact_id/delete', async (req, res) => {
+    const contactId = req.params.contact_id;
+    await db.delete(users).where(eq(users.id, Number(contactId)));
     return res.redirect(301, '/contacts');
+});
+
+contacts.get('/:contact_id', async (req, res) => {
+    const contactId = req.params.contact_id;
+
+    const user = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, Number(contactId)));
+
+    return res
+        .status(200)
+        .render('contacts/contact_details', { contact: user[0] });
 });

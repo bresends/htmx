f36b53bffffe7ce426047258155c82814efcf547
bruno <bruno.resende@gmx.com>
2024-02-12 18:25:09 -0300

    feat: added morgan

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import { Liquid } from 'liquidjs';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { root } from './router/root';
+import morgan from 'morgan';
 
 const app = express();
 
 app.engine('liquid', new Liquid().express());
 app.set('views', './src/views');
 app.set('view engine', 'liquid');
+
+app.use(morgan('dev'));
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));

3833048ee55d5531c52862ef3685cf14b5603093
bruno <bruno.resende@gmx.com>
2024-02-24 12:22:01 -0300

    feat: add jwt libs

--- env.ts
+++ env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  PORT: z.string().default('8080'),
-  DATABASE_URL: z.string().url(),
+    PORT: z.string().default('8080'),
+    DATABASE_URL: z.string().url(),
+    JWT_SECRET: z.string().min(32),
 });
 
 export const env = envSchema.parse(process.env);
